907b59c4bca87ede22e3ed8a768d50bc734525c3
ragel <shakilla.ragelia25@gmail.com>
2025-05-11 22:21:15 +0700

    update

--- app/Models/wisata.php
+++ app/Models/wisata.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class wisata extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'id_wisata',
+        'nama',
+        'slug',
+
+    ];
 }
