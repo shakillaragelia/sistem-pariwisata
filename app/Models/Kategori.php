17fea9d71692e64d960f6037c1612c786aa605fb
ragel <shakilla.ragelia25@gmail.com>
2025-05-16 19:56:56 +0700

    solve kategori and wisata relation

--- app/Models/Kategori.php
+++ app/Models/Kategori.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Kategori extends Model
 {
     protected $table = 'kategoris';
+    protected $primaryKey = 'id_kategori';
+
     protected $fillable = ['id_kategori','nama', 'slug', 'gambar'];
 
 }
