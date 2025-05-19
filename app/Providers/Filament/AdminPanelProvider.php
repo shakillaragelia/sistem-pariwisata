1805d806c22a840f27261900eb14481e1bc76d5e
ragel <shakilla.ragelia25@gmail.com>
2025-05-20 01:31:18 +0700

    add register

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,8 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()            
+            ->login()      
+            ->registration()      
             ->colors([
                 'primary' => Color::Amber,
             ])
