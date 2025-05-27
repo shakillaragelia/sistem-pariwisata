89a6d4c70397bdd2ff54dfe480b8af47de9b4305
ragel <shakilla.ragelia25@gmail.com>
2025-05-27 17:53:52 +0700

    fixed bug at total wisata - dashboard admin

--- app/Filament/Resources/KomentarResource.php
+++ app/Filament/Resources/KomentarResource.php
@@ -12,6 +12,11 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+
+
 
 class KomentarResource extends Resource
 {
@@ -33,7 +38,6 @@ class KomentarResource extends Resource
                     ->label('User')
                     ->relationship('user', 'name') 
                     ->required(),
-
             ]);
     }
 
@@ -48,12 +52,18 @@ class KomentarResource extends Resource
                     Tables\Columns\TextColumn::make('user.name')
                         ->label('Nama User'),
         
-                    Tables\Columns\TextColumn::make('wisata.nama')
-                        ->label('Nama Wisata'),
-        
+                    Tables\Columns\TextColumn::make('commentable.nama')
+                        ->label('Nama Wisata')
+                        ->sortable()
+                        ->searchable(),
+                    
                     Tables\Columns\TextColumn::make('created_at')
                         ->label('Dibuat')
                         ->dateTime(),
+
+                    Tables\Columns\TextColumn::make('commentable_type')
+                        ->label('Tipe Objek')
+                        ->formatStateUsing(fn ($state) => class_basename($state)),
         
             ])
             ->filters([
--- app/Filament/Widgets/StatistikOverview.php
+++ app/Filament/Widgets/StatistikOverview.php
@@ -7,6 +7,8 @@ use Filament\Widgets\StatsOverviewWidget\Card;
 use App\Models\User;
 use App\Models\visit;
 use App\Models\wisata;
+use App\Models\senbud;
+use App\Models\kuliner;
 use App\Models\event;
 use App\Models\hotel;
 use App\Models\kritiksaran;
@@ -16,12 +18,13 @@ class StatistikOverview extends BaseWidget
     protected function getCards(): array
     {
         return [
-            Card::make('Total Wisata', wisata::count()),
+            Card::make('Total Wisata', Wisata::count() + Kuliner::count() + Senbud::count()),
             Card::make('Total Event', event::count()),
             Card::make('Total Hotel', hotel::count()),
             Card::make('Total Pengguna', User::where('role', 'user')->count()),
             Card::make('Total Kritik Saran', kritiksaran::count()),
             Card::make('Total Pengunjung (Visit)', visit::count()),
         ];
+
     }
 }
