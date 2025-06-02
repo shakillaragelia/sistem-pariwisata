419100a6a07caeec0a1e580db6d3b6950572a484
ragel <shakilla.ragelia25@gmail.com>
2025-06-02 20:24:23 +0700

    disabled create & create another at senbud

--- app/Filament/Resources/SenbudResource/Pages/CreateSenbud.php
+++ app/Filament/Resources/SenbudResource/Pages/CreateSenbud.php
@@ -3,10 +3,11 @@
 namespace App\Filament\Resources\SenbudResource\Pages;
 
 use App\Filament\Resources\SenbudResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateSenbud extends CreateRecord
 {
     protected static string $resource = SenbudResource::class;
+
+    protected static bool $canCreateAnother = false;
 }
