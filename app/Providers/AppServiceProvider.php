9303113afaefed05fd9166c24cca8d6872c4321c
Shakilla Ragelia <shakilla.ragelia25@gmail.com>
2026-03-06 00:23:48 +0700

    refactor: remove request

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,20 +3,16 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Providers\FilamentGeocodeServiceProvider;
-use App\Models\Visit;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        
     }
 
     public function boot(): void
     {
-        if (!app()->runningInConsole() && !app()->runningUnitTests()) {
-            // Logic moved to LogVisit middleware
-        }
+       
     }
 }
