b4ffa31f67ee02cb15900de2096e679a0da74320
Shakilla Ragelia <shakilla.ragelia25@gmail.com>
2026-03-07 18:04:09 +0700

    Remove duplication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,7 @@
   <!-- <script src="https://unpkg.com/lucide@latest"></script> -->
 
   <!-- Tambahkan di <head> -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> -->
 
 <!-- Tambahkan sebelum </body> -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
