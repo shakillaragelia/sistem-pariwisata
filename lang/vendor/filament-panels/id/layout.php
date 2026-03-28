1294794747b41b7a0dedfc0326bc4deea0770794
Shakilla Ragelia <shakilla.ragelia25@gmail.com>
2026-03-28 18:29:22 +0700

    consistency at language

---
+++ lang/vendor/filament-panels/id/layout.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'direction' => 'ltr',
+    'buttons' => [
+        'sidebar' => [
+            'collapse' => 'Tutup Sidebar',
+            'expand' => 'Buka Sidebar',
+        ],
+    ],
+    'user_menu' => [
+        'profile_item' => [
+            'label' => 'Profil',
+        ],
+        'logout_button' => [
+            'label' => 'Keluar',
+        ],
+    ],
+];
---
+++ lang/vendor/filament-panels/id/pages/auth/login.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'title' => 'Masuk',
+    'heading' => 'Masuk ke Admin',
+    'buttons' => [
+        'submit' => [
+            'label' => 'Masuk',
+        ],
+    ],
+    'form' => [
+        'email' => [
+            'label' => 'Email',
+        ],
+        'password' => [
+            'label' => 'Kata Sandi',
+        ],
+        'remember' => [
+            'label' => 'Ingat saya',
+        ],
+    ],
+];
